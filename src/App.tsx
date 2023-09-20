45a5160141d833285d215673f1cee270b2c0068a
Sami Kashbi <sami.kashbi@microfocus.com>
2023-09-20 08:55:23 +0300

    * Initial FSM implementation. * Todo:
    - Code cleanup
    - Refactor the FSM init code
    - Refactor the Quiz summary
    - Added Quiz stats

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
+// src/App.tsx
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Quiz from './components/Quiz';
+import {ThemeProvider, createTheme} from '@mui/material/styles';
+import FSM from './components/FSM';
+
+const theme = createTheme();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <div className="App">
+                <Quiz/>
+            </div>
+        </ThemeProvider>
+    );
 }
 
 export default App;
